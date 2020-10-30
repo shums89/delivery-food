9ccce001449f10119bd0d76ca9e60b00b496740a
Александр <sh889@ya.ru>
2020-10-30 11:24:30 +0500

    День 3. AJAX, JSON, fetch

--- js/main.js
+++ js/main.js
@@ -26,6 +26,16 @@ const cardsMenu = document.querySelector('.cards-menu');
 
 let login = localStorage.getItem('gloDelivery');
 
+const getData = async function (url) {
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    throw new Error(`Ошибка по адресу "${url}", статус ошибки ${response.status}!`);
+  }
+
+  return await response.json();
+}
+
 function validName(str) {
   // Имя пользователя (с ограничением 2-20 символов, которыми могут быть буквы и цифры, первый символ обязательно буква)
   const regNName = /^[a-zA-Z][a-zA-Z0-9-_\.]{1,20}$/;
@@ -130,33 +140,36 @@ function checkAuth() {
   }
 }
 
-function createCardRestaurant() {
+function createCardRestaurant(restaurant) {
+  const { image, kitchen, name, price, products, stars, ime_of_delivery: timeOfDelivery } = restaurant;
+
   const card = `
-    <a class="card card-restaurant">
-      <img src="img/tanuki/preview.jpg" alt="image" class="card-image" />
+    <a class="card card-restaurant data-products="${products}"">
+      <img src=${image} alt="image" class="card-image" />
       <div class="card-text">
         <div class="card-heading">
-          <h3 class="card-title">Тануки</h3>
-          <span class="card-tag tag">60 мин</span>
+          <h3 class="card-title">${name}</h3>
+          <span class="card-tag tag">${timeOfDelivery}</span>
         </div>
         <div class="card-info">
           <div class="rating">
-            4.5
+            ${stars}
           </div>
-          <div class="price">От 1 200 ₽</div>
-          <div class="category">Суши, роллы</div>
+          <div class="price">От ${price} ₽</div>
+          <div class="category">${kitchen}</div>
         </div>
       </div>
     </a>
   `;
 
   cardsRestaurants.insertAdjacentHTML('beforeend', card);
-
 }
 
-function createCardGood() {
+function createCardGood(goods) {
   const card = document.createElement('div');
   card.className = 'card';
+
+  const { description, image, name, price } = goods;
 
   card.insertAdjacentHTML('beforeend', `
     <img src="img/pizza-plus/pizza-classic.jpg" alt="image" class="card-image" />
@@ -188,59 +201,65 @@ function openGoods(evt) {
     if (restaurant) {
       cardsMenu.textContent = '';
 
+      // console.log(restaurant);
+      // console.log(restaurant.dataset);
+
       containerPromo.classList.add('hide');
       restaurants.classList.add('hide');
       menu.classList.remove('hide');
 
-      createCardGood();
-      createCardGood();
-      createCardGood();
+      getData(`./db/${restaurant.dataset.products}`).then(function (data) {
+        data.forEach(createCardGood);
+      });
     }
   } else {
     toggleModalAuth();
   }
 }
 
-cartButton.addEventListener("click", toggleModal);
-close.addEventListener("click", toggleModal);
-cardsRestaurants.addEventListener('click', openGoods);
+function init() {
+  getData(`./db/partners.json`).then(function (data) {
+    data.forEach(createCardRestaurant);
+  });
 
-logo.addEventListener('click', function () {
-  containerPromo.classList.remove('hide')
-  restaurants.classList.remove('hide')
-  menu.classList.add('hide')
-});
+  cartButton.addEventListener("click", toggleModal);
+  close.addEventListener("click", toggleModal);
+  cardsRestaurants.addEventListener('click', openGoods);
 
-checkAuth();
+  logo.addEventListener('click', function () {
+    containerPromo.classList.remove('hide')
+    restaurants.classList.remove('hide')
+    menu.classList.add('hide')
+  });
 
-createCardRestaurant();
-createCardRestaurant();
-createCardRestaurant();
+  checkAuth();
 
-// Слайдер
+  // Слайдер
+  new Swiper('.swiper-container', {
+    sliderPerView: 1,
+    loop: true,
+    autoplay: true,
+    /*
+    grapCursor: true,
+    effect: 'cube',
+    cubeEffect: {
+      shadow: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    */
+    effect: 'coverflow',
+    scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+    },
+  })
+}
 
-new Swiper('.swiper-container', {
-  sliderPerView: 1,
-  loop: true,
-  autoplay: true,
-  /*
-  grapCursor: true,
-  effect: 'cube',
-  cubeEffect: {
-    shadow: false,
-  },
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  */
-  effect: 'coverflow',
-  scrollbar: {
-    el: '.swiper-scrollbar',
-    draggable: true,
-  },
-})
+init();
