11d29e2c45d17c74f7ea8300de69ea8e2cb55f52
Александр <sh889@ya.ru>
2020-10-30 15:03:24 +0500

    День 4. Переписываем код темы

--- js/main.js
+++ js/main.js
@@ -27,6 +27,9 @@ const restaurantRating = document.querySelector('.rating')
 const restaurantPrice = document.querySelector('.price')
 const restaurantcategory = document.querySelector('.category')
 const inputSearch = document.querySelector('.input-search')
+const modalBody = document.querySelector('.modal-body');
+const modalPrice = document.querySelector('.modal-pricetag');
+const buttonClearCart = document.querySelector('.clear-cart');
 
 /*
 // Создает ссылки-элементы из списка
@@ -39,6 +42,8 @@ str.split('\n').map(string => string.trim()).forEach(item => {
 */
 
 let login = localStorage.getItem('gloDelivery');
+
+const cart = [];
 
 const getData = async function (url) {
   const response = await fetch(url);
@@ -85,6 +90,12 @@ function clearForm() {
   loginPassword.style.borderColor = '';
 }
 
+function returnMain() {
+  containerPromo.classList.add('hide');
+  restaurants.classList.add('hide');
+  menu.classList.remove('hide');
+}
+
 function authorized() {
   function logOut() {
     login = null;
@@ -97,13 +108,15 @@ function authorized() {
     buttonOut.removeEventListener('click', logOut);
 
     checkAuth();
+    returnMain();
   }
 
   userName.textContent = login;
 
   buttonAuth.style.display = 'none';
   userName.style.display = 'inline';
-  buttonOut.style.display = 'block';
+  buttonOut.style.display = 'flex';
+  cartButton.style.display = 'flex';
 
   buttonOut.addEventListener('click', logOut);
 }
@@ -185,8 +198,7 @@ function createCardRestaurant(restaurant) {
 function createCardGood(goods) {
   const card = document.createElement('div');
   card.className = 'card';
-
-  const { description, image, name, price } = goods;
+  const { description, image, name, price, id } = goods;
 
   card.insertAdjacentHTML('beforeend', `
     <img src=${image} alt=${name} class="card-image" />
@@ -198,11 +210,11 @@ function createCardGood(goods) {
         <div class="ingredients">${description}</div>
       </div>
       <div class="card-buttons">
-        <button class="button button-primary button-add-cart">
+        <button class="button button-primary button-add-cart" id="${id}">
           <span class="button-card-text">В корзину</span>
           <span class="button-cart-svg"></span>
         </button>
-        <strong class="card-price-bold">${price} ₽</strong>
+        <strong class="card-price card-price-bold">${price} ₽</strong>
       </div>
     </div>
   `);
@@ -217,9 +229,7 @@ function openGoods(evt) {
   if (login) {
     if (restaurant) {
       cardsMenu.textContent = '';
-      containerPromo.classList.add('hide');
-      restaurants.classList.add('hide');
-      menu.classList.remove('hide');
+      returnMain();
 
       const { kitchen, name, price, stars } = restaurant.info;
 
@@ -240,20 +250,99 @@ function openGoods(evt) {
   }
 }
 
+function addToCart(evt) {
+  const target = evt.target;
+  const buttonAddToCart = target.closest('.button-add-cart');
+
+  if (buttonAddToCart) {
+    const card = target.closest('.card');
+    const title = card.querySelector('.card-title-reg').textContent;
+    const cost = card.querySelector('.card-price').textContent;
+    const id = buttonAddToCart.id;
+
+    const food = cart.find(function (item) {
+      return item.id === id;
+    });
+
+    if (food) {
+      food.count++;
+    } else {
+      cart.push({ id, title, cost, count: 1 });
+    }
+  }
+}
+
+function renderCart() {
+  modalBody.textContent = '';
+
+  cart.forEach(function ({ id, title, cost, count }) {
+    const itemCart = `
+    <div class="food-row">
+      <span class="food-name">${title}</span>
+      <strong class="food-price">${cost}</strong>
+      <div class="food-counter">
+        <button class="counter-button counter-minus" data-id=${id}>-</button>
+        <span class="counter">${count}</span>
+        <button class="counter-button counter-plus" data-id=${id}>+</button>
+      </div>
+    </div>
+    `;
+
+    modalBody.insertAdjacentHTML('beforeend', itemCart);
+  });
+
+  const totalPrice = cart.reduce(function (result, item) {
+    return result + (parseFloat(item.cost) + item.count);
+  }, 0);
+
+  modalPrice.textContent = `${totalPrice} ₽`;
+}
+
+function changeCount(evt) {
+  const target = evt.target;
+  console.log(target);
+
+  if (target.classList.contains('counter-button')) {
+    const food = cart.find(function (item) {
+      return item.id === target.dataset.id;
+    });
+
+    if (target.classList.contains('counter-minus')) {
+      food.count--;
+
+      if (food.count === 0) {
+        cart.splice(cart.indexOf(food), 1);
+      }
+    };
+
+    if (target.classList.contains('counter-plus')) {
+      food.count++;
+    }
+
+    renderCart();
+  }
+}
+
 function init() {
   getData(`./db/partners.json`).then(function (data) {
     data.forEach(createCardRestaurant);
   });
 
-  cartButton.addEventListener("click", toggleModal);
-  close.addEventListener("click", toggleModal);
-  cardsRestaurants.addEventListener('click', openGoods);
-
-  logo.addEventListener('click', function () {
-    containerPromo.classList.remove('hide')
-    restaurants.classList.remove('hide')
-    menu.classList.add('hide')
+  cartButton.addEventListener("click", function () {
+    renderCart();
+    toggleModal();
   });
+
+  buttonClearCart.addEventListener('click', function () {
+    cart.length = 0;
+    renderCart();
+  });
+
+  modalBody.addEventListener('click', changeCount);
+  cardsMenu.addEventListener('click', addToCart);
+  close.addEventListener('click', toggleModal);
+  cardsRestaurants.addEventListener('click', openGoods);
+  logo.addEventListener('click', returnMain);
 
   checkAuth();
 
@@ -316,9 +405,7 @@ function init() {
                   return name.includes(value.toLowerCase());
                 });
 
-                containerPromo.classList.add('hide');
-                restaurants.classList.add('hide');
-                menu.classList.remove('hide');
+                returnMain();
 
                 restaurantTitle.textContent = 'Результат поиска';
                 restaurantRating.textContent = '';
